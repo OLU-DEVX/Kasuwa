cd9d4b91061dcdd0d2fc721b2787a78ae76c97da
ShedrachJonah11 <shedrachjonah11@gmail.com>
2023-06-28 10:00:00 +0100

    Integrate toast notification component in app layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
+//million-ignore
 "use client";
+import * as React from "react";
+import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
 import Nav from "@/components/nav";
+import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import Footer from "@/components/footer";
 import { AppContext } from "@/utils/AppContext";
 import Head from "next/head";
+import Notification from "@/components/notification";
 import "../app/globals.css";
 
 function App({ Component, pageProps }: AppProps) {
@@ -17,7 +22,33 @@ function App({ Component, pageProps }: AppProps) {
   const [notificationVisibles, setNotificationVisible] = useState(false);
   const [list, setList] = useState([]);
   const API_URL = "https://kasuwa-b671.onrender.com";
+  // Function to save cart items to local storage
+  const saveCartItems = (cartItems: any) => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  };
 
+  // Function to load cart items from local storage
+  const loadCartItems = () => {
+    const savedCartItems =
+      typeof window !== "undefined"
+        ? window.localStorage.getItem("cartItems")
+        : false;
+    return savedCartItems ? JSON.parse(savedCartItems) : [];
+  };
+
+  // Function to save saved items to local storage
+  const saveSavedItems = (savedItems: any) => {
+    localStorage.setItem("savedItems", JSON.stringify(savedItems));
+  };
+
+  // Function to load saved items from local storage
+  const loadSavedItems = () => {
+    const savedItems =
+      typeof window !== "undefined"
+        ? window.localStorage.getItem("savedItems")
+        : false;
+    return savedItems ? JSON.parse(savedItems) : [];
+  };
   const FetchProducts = async () => {
     try {
       const allProduct = await fetch(`${API_URL}/products`);
@@ -27,19 +58,39 @@ function App({ Component, pageProps }: AppProps) {
       // Error handled silently
     }
   };
-
   useEffect(() => {
     FetchProducts();
   }, []);
-
   const showNotification = (message: any) => {
     setNotification(message);
     setNotificationVisible(true);
     setTimeout(() => {
       setNotification("");
       setNotificationVisible(false);
-    }, 3000);
+    }, 3000); // Hide the notification after 3 seconds (adjust the duration as needed)
   };
+
+  useEffect(() => {
+    // Load cart items and saved items from local storage on component mount
+    const loadedCartItems = loadCartItems();
+    const loadedSavedItems = loadSavedItems();
+
+    // Set the loaded items in state using the functional form of the state-setting functions
+    setCartItems((prevCartItems: any) => [
+      ...prevCartItems,
+      ...loadedCartItems,
+    ]);
+    setSavedItems((prevSavedItems: any) => [
+      ...prevSavedItems,
+      ...loadedSavedItems,
+    ]);
+  }, []);
+
+  useEffect(() => {
+    // Save cart items and saved items to local storage whenever they change
+    saveCartItems(cartItems);
+    saveSavedItems(savedItems);
+  }, [cartItems, savedItems]);
 
   const addToCart = (product: any, count: number) => {
     const itemWithCount = { ...product, quantity: count };
@@ -47,7 +98,6 @@ function App({ Component, pageProps }: AppProps) {
     showNotification(product.name);
     setNotificationAction("added to cart");
   };
-
   const addToSavedItems = (product: any) => {
     setSavedItems([...savedItems, product]);
     showNotification(product.title);
@@ -73,56 +123,64 @@ function App({ Component, pageProps }: AppProps) {
   };
 
   const increaseQuantity = (index: number) => {
+    // Create a new array with the updated quantity for the specific item
     const updatedCartItems = cartItems.map((item: any, i: any) =>
       i === index ? { ...item, quantity: item.quantity + 1 } : item
     );
     setCartItems(updatedCartItems);
   };
-
   const decreaseQuantity = (index: number) => {
+    // Create a new array with the updated quantity for the specific item
     const updatedCartItems = cartItems.map((item: any, i: any) =>
       i === index ? { ...item, quantity: item.quantity - 1 } : item
     );
     setCartItems(updatedCartItems);
   };
-
+  const router = useRouter();
   return (
-    <AppContext.Provider
-      value={{
-        cartItems,
-        setCartItems,
-        addToCart,
-        list,
-        removeFromCart,
-        isNavOpen,
-        setIsNavOpen,
-        count,
-        setCount,
-        increaseQuantity,
-        decreaseQuantity,
-        savedItems,
-        setSavedItems,
-        addToSavedItems,
-        removeFromSavedItems,
-        showNotification,
-        setNotification,
-        setNotificationAction,
-      }}
-    >
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Kasuwa</title>
-        <link
-          rel="icon"
-          href="/icon.svg?<generated>"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
-      </Head>
-      <Nav />
-      <Component {...pageProps} />
-      <Footer />
-    </AppContext.Provider>
+    <NextUIProvider>
+      <AppContext.Provider
+        value={{
+          cartItems,
+          setCartItems,
+          addToCart,
+          list,
+          removeFromCart,
+          isNavOpen,
+          setIsNavOpen,
+          count,
+          setCount,
+          increaseQuantity,
+          decreaseQuantity,
+          savedItems,
+          setSavedItems,
+          addToSavedItems,
+          removeFromSavedItems,
+          showNotification,
+          setNotification,
+          setNotificationAction,
+        }}
+      >
+        <Head>
+          {/* Define metadata for the app */}
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Kasuwa</title>
+          <link
+            rel="icon"
+            href="/icon.svg?<generated>"
+            type="image/<generated>"
+            sizes="<generated>"
+          />
+        </Head>
+        {!router.pathname.includes("auth/") &&
+          !router.pathname.includes("seller/sellerForm") && <Nav />}
+        {notificationVisibles && (
+          <Notification name={notification} action={notificationAction} />
+        )}
+        <Component {...pageProps} />
+        {!router.pathname.includes("auth/") && <Footer />}
+      </AppContext.Provider>
+    </NextUIProvider>
   );
 }
 
