a248b213b390b376e1b9f073b6a19eae276281b5
Ephraimiyanda <iyandaephraim@gmail.com>
2023-10-16 01:20:41 +0100

    searchbar:styling finished

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react"; // Import React and useState
 import {
   Navbar,
   NavbarBrand,
@@ -12,6 +13,12 @@ import {
   DropdownMenu,
   Badge,
   NavbarMenuToggle,
+  Modal, 
+  ModalContent, 
+  ModalHeader, 
+  ModalBody, 
+  ModalFooter,
+  useDisclosure
 } from "@nextui-org/react";
 import Image from "next/image";
 import logo from "../public/logo.svg";
@@ -28,13 +35,32 @@ import Orders from "../public/orders.svg";
 import { useContext } from "react";
 import { AppContext } from "@/utils/AppContext";
 import { useRouter } from "next/router";
+import logout from "../public/logout copy.svg";
+
 export default function Nav() {
+  const [isDropdownOpen, setDropdownOpen] = useState(false); // Use state to control the dropdown
   const router = useRouter();
   const { cartItems, list, setIsNavOpen, isNavOpen } = useContext(AppContext);
+  const userDetails =
+    typeof window !== "undefined" ? window.localStorage.getItem("user") : false;
+  const user = JSON.parse(userDetails as string);
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
+  const handleDropdownClick = () => {
+    if (!user) {
+      // If no user is logged in, redirect to the Sign In page
+      router.push("/auth/signIn");
+    } else {
+      // If a user is logged in, toggle the dropdown
+      setDropdownOpen(!isDropdownOpen);
+    }
+  };
+
   return (
+    
     <Navbar
       maxWidth="xl"
-      className="justify-around shadow px-3 bg-white p-0 sm:px-6"
+      className="justify-around shadow px-3 bg-white p-0 sm:px-6 top-[0]"
     >
       {router.pathname.includes("/seller") && (
         <NavbarMenuToggle
@@ -51,38 +77,40 @@ export default function Nav() {
         href="/"
       >
         <Image src={logo} alt="logo" width={35} height={45} />
-        <p className="hidden md:flex text-3xl font-bold text-[#A46E05] ">
+        <p className="flex text-3xl font-bold text-[#A46E05] ">
           KASUWA
         </p>
       </NavbarBrand>
-      <NavbarContent className="sm:flex-grow-[1] flex-grow-[1.2]">
-        <NavbarItem className="bg-white rounded-md w-full border border-[#A46E05]">
+      <NavbarContent className="sm:flex-grow-[1] flex-grow-[1.2] ">
+        <NavbarItem className="bg-white rounded-md md:m-0 ml-auto border md:w-full w-[50px] cursor-pointer" onClick={onOpen}>
           <Input
-            startContent={<SearchIcon />}
-            className="text-black  w-full m-auto"
-            radius="full"
+            startContent={<SearchIcon/>}
+            className="text-black  w-full m-auto border border-[#A46E05] rounded-[7px] bg-white cursor-pointer"
+            radius="sm"
             style={{
               paddingTop: "7px",
               paddingBottom: "7px",
               width: "100%",
+              borderColor: "#A46E05",
+              background: "white",
+              cursor:"pointer"
             }}
             placeholder="Search"
+            
           />
         </NavbarItem>
-        <Button className="bg-[#A46E05BD] rounded-md px-3 py-[7px] text-white">
+        <Button className="bg-[#A46E05BD] rounded-md px-3 py-[6px] text-white hidden sm:flex">
           Search
         </Button>
       </NavbarContent>
       <NavbarContent
         as="div"
         justify="end"
-        style={{
-          flexGrow: "0.3",
-        }}
-        className=""
+        className="trigger sm:flex-grow-[0.4] flex-grow-[0.3]"
+        
       >
-        <Dropdown placement="bottom-end">
-          <DropdownTrigger>
+        <Dropdown className="" placement="bottom-end" onClick={handleDropdownClick}>
+          <DropdownTrigger  >
             <div className="flex gap-2 justify-between items-center cursor-pointer">
               <Image
                 className="transition-transform"
@@ -104,27 +132,30 @@ export default function Nav() {
           <DropdownMenu
             aria-label="Profile Actions"
             variant="flat"
-            className="bg-white rounded-md p-3 shadow"
+            className="bg-white rounded-md p-3 flex flex-col gap-[2px]"
           >
-
-            <DropdownItem className="myDropItem" key="analytics">
-              <Link href={"/auth/signIn"}>
-                <Button className="w-full text-white bg-[#A46E05BD] py-2 rounded-md ">
-                  Sign In
-                </Button>
-              </Link>
-            </DropdownItem>
-            <DropdownItem className="myDropItem" key="analytics">
-              <Link 
-              href={"/auth/signup"}
-              >
+            <DropdownItem variant="flat" className="p-0" key="sign in">
+            {!user && (
               
-              <Button className="w-full bg-[#A46E05BD] text-white py-2 rounded-md ">
-                Sign Up
-              </Button>
-              </Link>
+                <Link href={"/auth/signIn"}>
+                  <Button className="w-full text-white bg-[#A46E05BD] py-2 rounded-md">
+                    Sign In
+                  </Button>
+                </Link>
+            )}
             </DropdownItem>
-            <DropdownItem className="myDropItem" key="system">
+
+            <DropdownItem variant="flat" className=" p-0 mt-1" key="sign up">
+            {!user && (
+                <Link href={"/auth/signup"}>
+                  <Button className="w-full bg-[#A46E05BD] text-white py-2 rounded-md">
+                    Sign Up
+                  </Button>
+                </Link>
+            )}
+            </DropdownItem>
+
+            <DropdownItem variant="flat" className=" py-2 mt-1 " key="account">
               <div className="flex gap-1 justify-start items-center">
                 <Image src={account} alt="logo" width={20} height={20} />
                 <span>
@@ -132,7 +163,7 @@ export default function Nav() {
                 </span>{" "}
               </div>
             </DropdownItem>
-            <DropdownItem className="myDropItem" key="settings">
+            <DropdownItem variant="flat" className=" py-2" key="settings">
               <div className="flex gap-1 justify-start items-center">
                 <Image src={Orders} alt="logo" width={20} height={20} />
                 <span>
@@ -140,48 +171,109 @@ export default function Nav() {
                 </span>
               </div>
             </DropdownItem>
-            <DropdownItem className="myDropItem" key="sell">
+            <DropdownItem variant="flat" className=" py-2" key="saved items">
               <div className="flex gap-2 justify-start items-center">
                 <Image src={savedItems} alt="logo" width={20} height={20} />
                 <span>
                   {" "}
-                  <Link href={"#"}>Saved Items</Link>
+                  <Link href={"/savedItems"}>Saved Items</Link>
                 </span>
               </div>
             </DropdownItem>
-            <DropdownItem className="myDropItem flex sm:hidden h-[50px]">
-              <div className="flex gap-1 justify-start items-center">
-                <Badge
-                  className="bg-[#A46E05BD] text-white p-2 z-10"
-                  color="primary"
-                  content={cartItems.length}
-                  size="sm"
-                >
-                  <Image src={cart} alt="logo" width={24} height={20} />
-                </Badge>
+            <DropdownItem
+              variant="flat"
+              className="myDropItem flex sm:hidden   py-1"
+            >
+              <div className="flex gap-2 justify-start items-center">
+                {cartItems.length > 0 ? (
+                  <Badge
+                    className="bg-[#A46E05] bg-opacity-100 text-white p-1 pt-0 z-10 mt-[2px] opacity-100"
+                    color="primary"
+                    content={cartItems.length}
+                  >
+                    <Image src={cart} alt="logo" width={21} height={20} />
+                  </Badge>
+                ) : (
+                  <Image src={cart} alt="logo" width={21} height={20} />
+                )}
+
                 <span>
                   <Link href={"/cart"}>Cart</Link>
                 </span>
+              </div>
+            </DropdownItem>
+            <DropdownItem
+              onClick={() => {
+                localStorage.removeItem("user");
+                router.push("/");
+              }}
+              variant="flat"
+              className="text-danger"
+              key="logout"
+              color="danger"
+            >
+              <div className="flex gap-1 justify-start items-center">
+                <Image src={logout} alt="logo" width={21} height={20} />
+                <span>
+                  <Link href="/account">Logout</Link>
+                </span>{" "}
               </div>
             </DropdownItem>
           </DropdownMenu>
         </Dropdown>
       </NavbarContent>
       <NavbarItem className="hidden sm:flex">
-        <div className="flex gap-1 justify-start items-center">
-          <Badge
-            className="bg-[#A46E05BD]  text-white p-2"
-            color="primary"
-            content={cartItems.length}
-            size="sm"
-          >
-            <Image src={cart} alt="logo" width={20} height={20} />
-          </Badge>
+        <div className="flex gap-[6px] justify-start items-center">
+          {cartItems.length > 0 ? (
+            <Badge
+              className="bg-[#A46E05BD] text-white p-1 pt-0 z-10 mt-[1px]"
+              color="primary"
+              content={cartItems.length}
+              variant="flat"
+            >
+              <Image src={cart} alt="logo" width={21} height={20} />
+            </Badge>
+          ) : (
+            <Image src={cart} className="pt-0" alt="logo" width={21} height={20} />
+          )}
           <span>
             <Link href={"/cart"}>Cart</Link>
           </span>
         </div>
       </NavbarItem>
-    </Navbar>
+   
+
+    <Modal 
+        isOpen={isOpen} 
+        onOpenChange={onOpenChange}
+        placement="top"
+        backdrop="blur"
+        className=" min-h-[60vh] "
+        size={"3xl"}
+        
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                  <Input
+                  autoFocus
+                  endContent={
+                    <SearchIcon />
+                  }
+                  placeholder="search for products"
+                  variant="bordered"
+                  className="text-black mt-5"
+                /> 
+              </ModalHeader>
+              <ModalBody>
+             
+             
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+      </Navbar> 
   );
 }
